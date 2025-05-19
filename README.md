1fa9b101aec8abe73f8db689e87bd1d8cd2af24d
Zachava96 <40652111+Zachava96@users.noreply.github.com>
2025-05-18 21:19:22 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Show Precise Accuracy Text
+
+This mod for UNBEATABLE adds the precise timing text (the number next to the "PERFECT") for all hits.
+
+![Image showing the timing text "GOOD - 103"](ShowPreciseAccuracyText.png)
+
+## Compatible game versions
+
+- UNBEATABLE Demo
+- UNBEATABLE \[white label\]
+
+## Requirements
+
+- [BepInEx](https://github.com/BepInEx/BepInEx)
+
+## Installation
+
+1. Download and install BepInEx into your game directory (if you use [CustomBeatmaps](https://github.com/gold-me/CustomBeatmapsV4), you have this installed already)
+2. Run the game, then close it
+3. [Download this mod](https://github.com/Zachava96/ShowPreciseAccuracyText/releases)
+4. Merge the BepInEx folder from this mod with the BepInEx folder in your game directory
+5. Run the game
+
+## Configuration
+
+There's only one option, which is to show values for spike dodges. You can change this in the `BepInEx/config/net.zachava.showpreciseaccuracytext.cfg` file by changing `ShowSpikeAccuracy = false` to `ShowSpikeAccuracy = true`
+
+The comment in that file explains when the value will be shown.
---
+++ ShowPreciseAccuracyText.cs
@@ -0,0 +1,60 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Configuration;
+using HarmonyLib;
+using Rhythm;
+
+namespace ShowPreciseAccuracyText
+{
+    [BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
+    [BepInProcess("UNBEATABLE [DEMO].exe")]
+    [BepInProcess("UNBEATABLE [white label].exe")]
+    public class ShowPreciseAccuracyText : BaseUnityPlugin
+    {
+        public const string PLUGIN_GUID = "net.zachava.showpreciseaccuracytext";
+        public const string PLUGIN_NAME = "Show Precise Accuracy Text";
+        public const string PLUGIN_VERSION = "1.0.0";
+        internal static new ManualLogSource Logger;
+        public static ConfigEntry<bool> ShowSpikeAccuracy;
+
+        private void Awake()
+        {
+            Logger = base.Logger;
+            ShowSpikeAccuracy = Config.Bind(
+                "General",
+                "ShowSpikeAccuracy",
+                false,
+                "Show precise accuracy text for spike dodges when possible.\n" +
+                "If you dodge the spike outside the +-150ms window, it still won't show.\n" +
+                "For example, if you jump over the spike with an input >150ms before the spike\n" +
+                "or you dodge by standing under it."
+            );
+            Logger.LogInfo($"Plugin {PLUGIN_GUID} is loaded!");
+            var harmony = new Harmony(PLUGIN_GUID);
+            harmony.PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(AttackInfo))]
+    [HarmonyPatch("GetTierText")]
+    class AttackInfoPatches
+    {
+        static void Postfix(AttackInfo __instance, Score score, ref string __result)
+        {
+            // "PERFECT" with no precise accuracy text is a spike dodge
+            // This is normally replaced by "NICE" at a later point
+            // But we'll do it here (if we're showing spike dodges) so we can add the precise accuracy text in the next block
+            if (ShowPreciseAccuracyText.ShowSpikeAccuracy.Value && (__result == "PERFECT"))
+            {
+                __result = "NICE";
+            }
+            // If the score is not "PERFECT", add the precise accuracy text
+            // (so long as it's not a miss from completely missing the note)
+            // (if it's a miss from releasing a hold note too early, show the precise accuracy text)
+            if (!__result.StartsWith("PERFECT") && (__instance.GetPreciseAccuracy(score) < score.leniency))
+            {
+                __result += " " + __instance.GetPreciseAccuracyText(score);
+            }
+        }
+    }
+}
